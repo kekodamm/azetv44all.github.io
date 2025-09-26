6da658b9c34ffda7052b1227d8e656a818021c91
azetv44tool <egzozsokan31@gmail.com>
2025-09-26 19:58:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'azetv44 sxrgu klasör.rar'; // kendi dosyanın adını buraya koy
-    link.download = 'azetv44 sxrgu klasör.rar';
+    link.href = 'https://raw.githubusercontent.com/kekoadamm.github.io/azetv44all/main/azetv44 sxrgu klasör.rar.rar';
+    link.download = 'dosyan.zip';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 });
-
-
